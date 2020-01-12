a5525cb30f5447a5bc5d947cb96d4129aa842d4f
Josh <Jsmithrud37@gmail.com>
2020-01-11 19:07:25 -0800

    refactor: Changed page to have placeholder content for Star Wars campaign. Uses logo, and links to sw5e site.

--- datapad/src/App.tsx
+++ datapad/src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 /**
- *
+ * Entrypoint to the application. Outermost container.
  */
 const App: React.FC = () => {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<div className="stars">
+				<header className="App-header">
+					<img
+						src="images/Order-Of-The-Fallen-Logo-Long.png"
+						className="Campaign-logo"
+						alt="Campaign logo"
+						height="100"
+					/>
+					<a className="App-link" href="https://sw5e.com/" target="_blank" rel="noopener noreferrer">
+						<img
+							src="https://sw5e.com/img/sw5e-logo.84b4d7ed.png"
+							className="SW5e-logo"
+							alt="SW5e logo"
+							height="100"
+						/>
+					</a>
+				</header>
+			</div>
 		</div>
 	);
 };
