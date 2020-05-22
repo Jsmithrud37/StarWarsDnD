7deca5ed622d1f0fbd82c01bceefbb979206b830
Josh <Jsmithrud37@gmail.com>
2020-05-21 20:53:52 -0700

    feat: Added sub-menu selections

--- datapad/src/components/Datapad.tsx
+++ datapad/src/components/Datapad.tsx
@@ -4,6 +4,8 @@ import Card from 'react-bootstrap/card';
 import GalaxyMap from './GalaxyMap';
 import './Datapad.css';
 
+type CardColors = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light' | undefined;
+
 /**
  *Datapad main entry-point. Appears below header in app. Contains side-bar UI for navigating options.
  */
@@ -44,40 +46,14 @@ class Menu extends React.Component<{}, MenuState, any> {
 		return id === this.state.selection;
 	}
 
-	private bgColor(
-		id: string,
-	): 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light' | undefined {
+	private bgColor(id: string): CardColors {
 		return this.isSelected(id) ? 'primary' : 'dark';
 	}
 
 	render() {
 		return (
 			<Accordion className="Datapad-menu">
-				<Card bg={this.bgColor(galaxyMapId)} text="light">
-					<Accordion.Toggle
-						as={Card.Header}
-						eventKey={galaxyMapId}
-						onClick={() => this.setSelection(galaxyMapId)}
-					>
-						Galaxy Map
-					</Accordion.Toggle>
-				</Card>
-				<Card bg={this.bgColor(shopsId)} text="light">
-					<Accordion.Toggle as={Card.Header} eventKey={shopsId} onClick={() => this.setSelection(shopsId)}>
-						Shops
-					</Accordion.Toggle>
-					<Accordion.Collapse eventKey={shopsId}>
-						<div>
-							<Card bg="dark" text="light">
-								Equipment
-							</Card>
-							<Card bg="dark" text="light" border="primary">
-								Apothicary
-							</Card>
-						</div>
-					</Accordion.Collapse>
-				</Card>
-				<Card bg={this.bgColor(contactsId)} text="light">
+				{/* <Card bg={this.bgColor(contactsId)} text="light">
 					<Accordion.Toggle
 						as={Card.Header}
 						eventKey={contactsId}
@@ -90,7 +66,123 @@ class Menu extends React.Component<{}, MenuState, any> {
 							TODO
 						</Card>
 					</Accordion.Collapse>
-				</Card>
+				</Card> */}
+				<MenuItem
+					title="Galaxy Map"
+					id={galaxyMapId}
+					isSelected={this.isSelected(galaxyMapId)}
+					onClick={() => this.setSelection(galaxyMapId)}
+				/>
+				<CollapsableMenuItem
+					title="Shops"
+					id={shopsId}
+					isSelected={this.isSelected(contactsId)}
+					onClick={() => this.setSelection(contactsId)}
+					content={<InventorySubMenu />}
+				></CollapsableMenuItem>
+				<MenuItem
+					title="Contacts"
+					id={contactsId}
+					isSelected={this.isSelected(shopsId)}
+					onClick={() => this.setSelection(shopsId)}
+				/>
+			</Accordion>
+		);
+	}
+}
+
+interface MenuItemParameters {
+	title: string;
+	id: string;
+	isSelected: boolean;
+	onClick: () => void;
+}
+
+class MenuItem extends React.Component<MenuItemParameters> {
+	public constructor(props: MenuItemParameters) {
+		super(props);
+	}
+
+	private backgroundColor(): CardColors {
+		return this.props.isSelected ? 'primary' : 'dark';
+	}
+
+	render() {
+		return (
+			<Card bg={this.backgroundColor()} text="light">
+				<Accordion.Toggle as={Card.Header} eventKey={this.props.id} onClick={() => this.props.onClick()}>
+					{this.props.title}
+				</Accordion.Toggle>
+			</Card>
+		);
+	}
+}
+
+interface CollapsableMenuItemParameters extends MenuItemParameters {
+	content: any; // TODO: sub-items
+}
+
+class CollapsableMenuItem extends React.Component<CollapsableMenuItemParameters> {
+	public constructor(props: CollapsableMenuItemParameters) {
+		super(props);
+	}
+
+	private backgroundColor(): CardColors {
+		return this.props.isSelected ? 'primary' : 'dark';
+	}
+
+	render() {
+		return (
+			<Card bg={this.backgroundColor()} text="light">
+				<Accordion.Toggle as={Card.Header} eventKey={this.props.id} onClick={() => this.props.onClick()}>
+					{this.props.title}
+				</Accordion.Toggle>
+				<Accordion.Collapse eventKey={this.props.id}>
+					<Card bg="dark" text="light">
+						{this.props.content}
+					</Card>
+				</Accordion.Collapse>
+			</Card>
+		);
+	}
+}
+
+const equipmentId = '3';
+const apothicaryId = '4';
+
+class InventorySubMenu extends React.Component<{}, MenuState, any> {
+	public constructor(props: {}) {
+		super(props);
+		this.state = {
+			selection: equipmentId,
+		};
+	}
+
+	private setSelection(id: string) {
+		this.setState({
+			selection: id,
+		});
+	}
+
+	private isSelected(id: string): boolean {
+		return id === this.state.selection;
+	}
+
+	render() {
+		return (
+			<Accordion>
+				<MenuItem
+					title="Equipment"
+					id={equipmentId}
+					isSelected={this.isSelected(equipmentId)}
+					onClick={() => this.setSelection(equipmentId)}
+				/>
+				<MenuItem
+					title="Apothicary"
+					id={apothicaryId}
+					isSelected={this.isSelected(apothicaryId)}
+					onClick={() => this.setSelection(apothicaryId)}
+				/>
 			</Accordion>
 		);
 	}
