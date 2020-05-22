379f8aee104b2e413e2a3d9af3d8258c06a19e77
Josh <Jsmithrud37@gmail.com>
2020-05-21 19:36:00 -0700

    refactor: Made App a standard component

--- datapad/src/components/App.tsx
+++ datapad/src/components/App.tsx
@@ -5,30 +5,32 @@ import './App.css';
 /**
  * Entrypoint to the application. Outermost container.
  */
-const App: React.FC = () => {
-	return (
-		<body className="App">
-			<header className="App-header">
-				<img
-					src="images/Order-Of-The-Fallen-Logo-Long.png"
-					className="Campaign-logo"
-					alt="Campaign logo"
-					height="75"
-				/>
-				<a className="App-link" href="https://sw5e.com/" target="_blank" rel="noopener noreferrer">
+export class App extends React.Component {
+	render() {
+		return (
+			<body className="App">
+				<header className="App-header">
 					<img
-						src="https://sw5e.com/img/sw5e-logo.84b4d7ed.png"
-						className="SW5e-logo"
-						alt="SW5e logo"
+						src="images/Order-Of-The-Fallen-Logo-Long.png"
+						className="Campaign-logo"
+						alt="Campaign logo"
 						height="75"
 					/>
-				</a>
-			</header>
-			<body className="App-container">
-				<Datapad />
+					<a className="App-link" href="https://sw5e.com/" target="_blank" rel="noopener noreferrer">
+						<img
+							src="https://sw5e.com/img/sw5e-logo.84b4d7ed.png"
+							className="SW5e-logo"
+							alt="SW5e logo"
+							height="75"
+						/>
+					</a>
+				</header>
+				<body className="App-container">
+					<Datapad />
+				</body>
 			</body>
-		</body>
-	);
-};
+		);
+	}
+}
 
 export default App;
