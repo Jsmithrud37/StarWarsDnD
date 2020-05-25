9cc6f97511cc7a1caba09a76071201fce37a2dfe
Josh <Jsmithrud37@gmail.com>
2020-05-25 14:33:49 -0700

    improvement: Removed some unneeded UI elements from galaxy map embed

--- src/apps/GalaxyMap/GalaxyMap.tsx
+++ src/apps/GalaxyMap/GalaxyMap.tsx
@@ -7,7 +7,7 @@ import Iframe from 'react-iframe';
 const GalaxyMap: React.FC = () => {
 	return (
 		<Iframe
-			url="https://hbernberg.cartodb.com/viz/76e286d4-fbab-11e3-b014-0e73339ffa50/embed_map?title=false&amp;description=true&amp;search=false&amp;shareable=false&amp;cartodb_logo=false&amp;layer_selector=true&amp;legends=true&amp;scrollwheel=true&amp;fullscreen=true&amp;sublayer_options=1%7C1%7C1%7C1&amp;sql=&amp;zoom=3&amp;center_lat=0&amp;center_lon=0"
+			url="https://hbernberg.cartodb.com/viz/76e286d4-fbab-11e3-b014-0e73339ffa50/embed_map?title=false&amp;description=false&amp;search=false&amp;shareable=false&amp;cartodb_logo=false&amp;layer_selector=true&amp;legends=false&amp;scrollwheel=true&amp;fullscreen=false&amp;sublayer_options=1%7C1%7C1%7C1&amp;sql=&amp;zoom=3&amp;center_lat=0&amp;center_lon=0"
 			width="100%"
 			height="100%"
 			id="galaxy-map-frame"
