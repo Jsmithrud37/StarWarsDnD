98d4d6c64f078e6a17ea266a4b68c45355d22e77
Josh <Jsmithrud37@gmail.com>
2020-06-20 22:52:28 -0700

    test: Fixed test

--- src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../apps/App';
-
-test('renders learn react link', () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/Datapad/i);
-	expect(linkElement).toBeInTheDocument();
-});
