31128cc50e8522c660848f77f9ea0f82d16c9b81
Josh <Jsmithrud37@gmail.com>
2020-07-11 16:50:47 -0700

    improvement: Refactored hello-world function a bit

--- backend/hello-world.ts
+++ backend/hello-world.ts
@@ -6,10 +6,10 @@ const helloWorldHandler: Handler<APIGatewayEvent, APIGatewayProxyResult> = async
 	event: APIGatewayEvent,
 ): Promise<APIGatewayProxyResult> => {
 	try {
-		const subject = event.queryStringParameters.name || 'World';
+		const message = createMessage(event.queryStringParameters.name);
 		return {
 			statusCode: 200,
-			body: JSON.stringify({ message: `Hello ${subject}` }),
+			body: JSON.stringify({ message }),
 			// // more keys you can return:
 			// headers: { "headerName": "headerValue", ... },
 			// isBase64Encoded: true,
@@ -18,5 +18,12 @@ const helloWorldHandler: Handler<APIGatewayEvent, APIGatewayProxyResult> = async
 		return { statusCode: 500, body: err.toString() };
 	}
 };
+
+/**
+ * Generates the hello message to the specified subject.
+ */
+function createMessage(subject?: string): string {
+	return `Hello ${subject ?? 'World'}`;
+}
 
 exports.handler = helloWorldHandler;
