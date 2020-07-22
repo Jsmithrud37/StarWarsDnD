b39a231d33df0677a284e2d0bdbbfab01ae2ae35
Joshua Smithrud <Jsmithrud37@gmail.com>
2020-07-21 22:39:10 -0700

    chore: Removed console logging

--- backend/utilities/LocalDbConnect.ts
+++ backend/utilities/LocalDbConnect.ts
@@ -6,10 +6,7 @@ import * as Path from 'path';
  */
 export function localDbUrl(): string {
 	const filePath = Path.resolve('db_url.txt');
-	console.log(`Reading file...`);
 	const fileContents = Fs.readFileSync(filePath, { encoding: 'utf8' });
-	console.log('Finished reading file :)');
 	const url = fileContents.trim();
-	console.log(url);
 	return url;
 }
