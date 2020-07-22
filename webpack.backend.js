7827df9037e498132102006358e247740ce4f170
Joshua Smithrud <Jsmithrud37@gmail.com>
2020-07-21 22:38:46 -0700

    fix: Different attempt at fixing webpack issue

--- webpack.backend.js
+++ webpack.backend.js
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const webpack = require('webpack');
 
 /**
  * Additional webpack config properties for use with the Netlify backend.
  */
 module.exports = {
 	plugins: [
-		new FilterWarningsPlugin({
-			// Works around issues in Mongoose: <https://github.com/Automattic/mongoose/issues/7476>
-			exclude: /Critical dependency: the request of a dependency is an expression/,
-		}),
+		// Works around issues in Mongoose: <https://github.com/Automattic/mongoose/issues/7476>
+		new webpack.ContextReplacementPlugin(/require_optional/),
+		new webpack.ContextReplacementPlugin(/mongoose/),
 	],
 };
