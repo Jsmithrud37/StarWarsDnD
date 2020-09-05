b414918d8b8fc6d2bf5b555ab987207d09dd0f3d
Joshua Smithrud <Jsmithrud37@gmail.com>
2020-09-05 14:35:20 -0700

    fix: Fixed backend build

--- backend/utilities/LocalDbConnect.ts
+++ backend/utilities/LocalDbConnect.ts
@@ -1,8 +1,9 @@
-import * as debugConfig from '../debug-environment.json';
+import * as Fs from 'fs-extra';
 
 /**
  * For local use only. Extract url with auth credentials from git-ignored file.
  */
 export function localDbUrl(): string {
-	return debugConfig.DB_URL;
+	const json = Fs.readJSONSync('../debug-environment.json');
+	return json.DB_URL;
 }
