611acb96cb960c35e33b1a5c82d21bb5a4739faf
Joshua Smithrud <Jsmithrud37@gmail.com>
2020-09-05 20:03:28 -0700

    fix: Fixed local debug workflow

--- backend/utilities/LocalDbConnect.ts
+++ backend/utilities/LocalDbConnect.ts
@@ -1,9 +1,10 @@
 import * as Fs from 'fs-extra';
+import * as Path from 'path';
 
 /**
  * For local use only. Extract url with auth credentials from git-ignored file.
  */
 export function localDbUrl(): string {
-	const json = Fs.readJSONSync('../debug-environment.json');
+	const json = Fs.readJSONSync(Path.resolve('backend-debug-environment.json'));
 	return json.DB_URL;
 }
