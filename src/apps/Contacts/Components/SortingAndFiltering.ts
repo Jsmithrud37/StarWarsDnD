44b773a78bbde587b7fcf8b31f075008191ab143
Joshua Smithrud <Jsmithrud37@gmail.com>
2021-02-27 19:50:18 -0800

    feat: Added character filter types

---
+++ src/apps/Contacts/Components/SortingAndFiltering.ts
@@ -0,0 +1,15 @@
+/**
+ * Sorting attribute
+ */
+export enum SortBy {
+	NameAscending = 'Name (A-Z)',
+	NameDescending = 'Name (Z-A)',
+	PCsFirst = 'PCs First',
+	NPCsFirst = 'NPCs First',
+}
+
+export enum CharacterKindFilter {
+	All = 'All',
+	PCs = 'Player Characters',
+	NPCs = 'NPCs',
+}
